e9ce5570cdc8d209ce926912d5f4735f39ccef1a
Shashank Ginjpalli <shashank.ginjpalli@gmail.com>
2019-12-05 22:16:45 -0700

    Added Error History functions and Error Creation functions

--- src/project/ErrorReporter.java
+++ src/project/ErrorReporter.java
@@ -4,4 +4,28 @@ package project;
 //not sure how we would like to structure this
 public class ErrorReporter {
 	
+	private String errorHistory = "";
+	
+	public String createError(String errorString, String lineNumber) {
+		String error = errorString + lineNumber;
+		appendToHistory(error);
+		
+		return error; 
+	}
+	
+	public String createError(String errorString) {
+		String error = errorString;
+		appendToHistory(error);
+		
+		return error;
+	}
+	
+	public void appendToHistory(String Error) {
+		errorHistory = errorHistory + Error + "\n";
+	}
+	
+	public String getErrorHistory() {
+		return errorHistory;
+	}
+	
 }
